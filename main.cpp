2c2036f9e2a2dba9b4c46a895996c81f84c34b85
nomadic_willow <nomadic_willow@iris.com>
2019-10-21 15:42:38 +0530

    DFS with parse trees

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 class pos{
 public:
-    char s;
+    string s;
     int x;
     int y;
-    pos(char str=NULL,int a=0,int b=0):s(str),x(a),y(b){} //constructor
+    pos(string str=NULL,int a=0,int b=0):s(str),x(a),y(b){} //constructor
 };
 
 string getString(char x)
@@ -15,17 +15,16 @@ string getString(char x)
     return string(1, x);
 }
 
-void traverse(pos p,vector<unordered_multimap<string,pair<pos,pos>>> graph[]){
+void traverse(pos p,vector<unordered_multimap<string,pos>> graph[]){
 //    cout<<p.s<<"("<<p.x<<","<<p.y<<") ";
     cout<<p.s<<" ";
     if(p.x == -1 && p.y == -1){
         return;
     }else{
         for(auto it1 = graph[p.x][p.y].begin(); it1 != graph[p.x][p.y].end(); it1++){
-            if(it1 -> first == getString(p.s)){
+            if(it1 -> first == (p.s)){
                 pos a,b;
-                a = it1->second.first;
-                b = it1->second.second;
+                a = it1->second;
 //                if(it1->second.second.x != -1)graph[p.x][p.y].erase(it1);
                 traverse(a,graph);
                 traverse(b,graph);
@@ -37,11 +36,12 @@ void traverse(pos p,vector<unordered_multimap<string,pair<pos,pos>>> graph[]){
 
 int main() {
     unordered_map<string,vector<string>> mp;
-    unordered_multimap<string,pair<pos,pos>> tmp_map,tmp_map1,tmp_map2;
+    unordered_multimap<string,pos> tmp_map,tmp_map1,tmp_map2;
     int i,j,k,t,l,n,size,x,y,x1,x2,y1,y2;
     vector<string> tmp;
-    string s,a,b,c,tmp_string;
+    string s,a,b,c,tmp_string,str;
     a = ".";
+    cout<<"Input (Provide each possibility as provided in example in input.txt):"<<endl;
     cin>>a>>b;
     while(a != "^^^"){
         if(mp.find(b) == mp.end()){
@@ -54,57 +54,28 @@ int main() {
     }
     s = b;
     l = s.length();
-    vector<unordered_multimap<string,pair<pos,pos>>> graph[l],tmp_graph[l];
+    vector<unordered_multimap<string,pos>> graph[l];
     for(i = 0; i < l; i++){
-        graph[i] = vector<unordered_multimap<string,pair<pos,pos>>>(l-i);
+        graph[i] = vector<unordered_multimap<string,pos>>(l-i);
     }
-    //1st row
+
+    //1st row of length 1 in pyramid
+    string literal = "$";
     for(i = 0; i < l; i++){
-        cout<<">>>>>>>>>>>>>>>>>>>>>>>>>Length: "<<1<<endl;
         if(mp.find(getString(s[i])) != mp.end()){
             tmp = mp[getString(s[i])];
-            cout<<"Found: "<<s[i]<<tmp.size()<<endl;
+//            cout<<"Found: "<<s[i]<<tmp.size()<<endl;
             for(j = 0; j < tmp.size(); j++){
-                graph[0][i].insert({tmp[j],{pos(s[i],-1,-1),pos('$',-1,-1)}}); // $ for sign of literal
+                //str is in VLR fromat for DFS traversal
+                str = tmp[j]+s[i]+literal;
+                graph[0][i].insert({tmp[j],pos(str,-1,-1)}); // $ for sign of literal
             }
-        }else{
-            cout<<"Not found: "<<s[i]<<endl;
-//            cout<<getString(s[i])<<endl;
         }
     }
-    //2nd row
-    /*for(i = 0; i < l - 1; i++){
-        cout<<">>>>>>>>>>>>>>>>>>>>>>>>>Length: "<<2<<endl;
-        x = i;
-        y = i + 1;
-        if(graph[0][x].size() != 0 && graph[0][y].size() != 0){
-            tmp_map1 = graph[0][x];
-            for(auto itt1 = tmp_map1.begin(); itt1 != tmp_map1.end(); itt1++){
-                tmp_map2 = graph[0][y];
-                for(auto itt2 = tmp_map2.begin(); itt2 != tmp_map2.end(); itt2++){
-                    tmp_string = itt1->first + itt2->first;
-
-                    //finding and adding
-                    if(mp.find(tmp_string) != mp.end()){
-                        tmp = mp[tmp_string];
-                        cout<<"Found: "<<tmp_string<<tmp.size()<<endl;
-                        for(j = 0; j < tmp.size(); j++){
-                            graph[1][i].insert({tmp[j],{pos(itt1->first[0],0,i),pos(itt2->first[0],0,i+1)}});
-                        }
-                    }else{
-                        cout<<"Not found: "<<tmp_string<<endl;
-//                        cout<<tmp_string<<endl;
-                    }
-                }
-            }
-        }
-    }*/
 
     //for remaining sized strings
     for(k = 1; k < l; k++){
-        cout<<">>>>>>>>>>>>>>>>>>>>>>>>>Length: "<<k+1<<endl;
         for(i = 0; i < l-k; i++){
-            cout<<">>>>>>>>>>>>>>>>>>>>>>>>>Col: "<<i+1<<endl;
             x1 = 0;
             y1 = i;
             x2 = k-1;
@@ -116,16 +87,17 @@ int main() {
                         tmp_map2 = graph[x2][y2];
                         for(auto itt2 = tmp_map2.begin(); itt2 != tmp_map2.end(); itt2++){
                             tmp_string = itt1->first + itt2->first;
-
                             //finding and adding
+                            //if found
                             if(mp.find(tmp_string) != mp.end()){
                                 tmp = mp[tmp_string];
-                                cout<<"Found: "<<tmp_string<<tmp.size()<<endl;
+//                                cout<<"Found: "<<tmp_string<<tmp.size()<<endl;
                                 for(j = 0; j < tmp.size(); j++){
-                                    graph[k][i].insert({tmp[j],{pos(itt1->first[0],x1,y1),pos(itt2->first[0],x2,y2)}});
+                                    //str is in VLR fromat for DFS traversal
+                                    //pos object defines how the object is formed
+                                    str = tmp[j]+itt1->second.s + itt2->second.s;
+                                    graph[k][i].insert({tmp[j],pos(str,x1,y1)});
                                 }
-                            }else{
-                                cout<<"Not found: "<<tmp_string<<endl;
                             }
                         }
                     }
@@ -151,60 +123,14 @@ int main() {
         }
         cout<<endl;
     }
-    cout<<"\nFinal : "<<graph[l-1][0].size()<<endl;
+    cout<<"\nNo. Of distinct Parse Trees : "<<graph[l-1][0].size()<<endl;
+    cout<<"\nParse Trees : "<<endl;
 
-    for(j = 0; j<1; j++){
-        cout<<graph[l-1][j].size()<<" ";
-        tmp_map = graph[l-1][j];
-        for(auto itt = tmp_map.begin(); itt != tmp_map.end(); itt++){
-            cout<<itt->first<<" ( ";
-            cout<<itt->second.first.s<<" , "<<itt->second.first.x<<" , "<<itt->second.first.y<<" )"<<" & ( "<<itt->second.second.s<<" , "<<itt->second.second.x<<" , "<<itt->second.second.y<<" )";
-            cout<<endl;
-        }
-    }
-    pos tmp_pos;
-    for(j = 0; j<1; j++){
-        for(auto itt = graph[l-1][j].begin(); itt != graph[l-1][j].end(); itt++){
-            queue<pos> q;
-            cout<<itt->first<<" ";
-//            graph[l-1][j].erase(itt);
-            traverse(itt->second.first,graph);
-            traverse(itt->second.second,graph);
-            /*q.push(itt->second.first);//pushing left entity
-            q.push(itt->second.second);//pushing right entity
-            while(q.size() != 0){
-                tmp_pos = q.front();
-                q.pop();
-                cout<<tmp_pos.s<<" ";
-                if(tmp_pos.x != -1 && tmp_pos.y != -1){
-                    for(auto it1 = graph[tmp_pos.x][tmp_pos.y].begin(); it1 != graph[tmp_pos.x][tmp_pos.y].end(); it1++){
-                        if(it1 -> first == getString(tmp_pos.s)){
-                            q.push(it1->second.first);//pushing left entity
-                            q.push(it1->second.second);//pushing right entity
-//                            graph[tmp_pos.x][tmp_pos.y].erase(it1);
-                            break;
-                        }
-                    }
-                }
-            }*/
-
-            cout<<endl;
-        }
-    }
-    cout<<"llllllll:"<<endl;
-    for(i = l-1; i >= 0; i--){
-//        g[i] = unordered_multimap<string,pair<pos,pos>>(i);
-        cout<<graph[i].size()<<endl;
-        cout<<"| ";
-        for(j = 0; j<l-i; j++){
-            cout<<graph[i][j].size()<<" ";
-            tmp_map = graph[i][j];
-            for(auto itt = tmp_map.begin(); itt != tmp_map.end(); itt++){
-                cout<<itt->first<<" ";
-            }
-            cout<<" | ";
-        }
-        cout<<endl;
+    //traversal
+    //in every node of pyramid, each literal is having the data of its path from base in "VLR" format
+    i=1;
+    for(auto itt = graph[l-1][0].begin(); itt != graph[l-1][0].end(); itt++,i++){
+        cout<<i<<": "<<itt->second.s<<endl;
     }
     return 0;
 }
